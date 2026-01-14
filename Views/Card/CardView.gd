c910155de84ea7c1bb9b068f5542a3b32b0fb793
Giuliano Mozzillo <giulianomozzillo@live.it>
2026-01-14 19:48:50 +0100

    🚀 Card look & feel added

--- Views/Card/CardView.gd
+++ Views/Card/CardView.gd
@@ -9,6 +9,9 @@ enum DIRECTIONS {
 	DOWN
 }
 
+@export var suit_atlas_source: Texture2D = preload("res://assets/suits.png")
+
+
 # Idle animation
 @export var bob_amplitude := 4.0        # px
 @export var bob_speed := 1.5            # rad/sec
@@ -24,23 +27,29 @@ enum DIRECTIONS {
 @export var reveal_shake_rot := 10.0
 
 # Aliases
-@onready var root: Control = %CardRoot
-@onready var sides: Control = %CardSides
+@onready var motion_base: Control = %MotionBase
+@onready var motion_idle: Control = %MotionIdle
+
 @onready var face: TextureRect = %CardFace
 @onready var back: TextureRect = %CardBack
 @onready var shadow: TextureRect = %Shadow
 @onready var rankLabel: Label = %RankLabel
+@onready var suitIcon: = %SuitIcon
+
+
+var _suit_atlas = AtlasTexture.new()
 
 # Animation helpers
 var _time := 0.0
 var _phase := randf() * TAU
 
-var _base_root_position: Vector2
-var _base_sides_position: Vector2
+var _base_motion_base_position: Vector2
 
 
 func _ready(): 
+	_suit_atlas.atlas = suit_atlas_source
 	pass
+
 	
 func _process(delta):
 	play_idle_animation(delta)
@@ -57,19 +66,32 @@ func bind(card_instance: CardInstance):
 func _update_visual():
 	# Set Rank Label -- temporary
 	rankLabel.text = card.data.get_rank()
+	_update_suit_icon()
 	
 	if card.is_face_down:
 		back.visible = true
 		face.visible = false
 		rankLabel.visible = false
+		suitIcon.visible = false
 	else:
 		back.visible = false
 		face.visible = true
 		rankLabel.visible = true
+		suitIcon.visible = true
 		#face.texture = CardAtlas.get_texture(
 			#card.data.suit,
 			#card.data.rank
 		#)
+
+func _update_suit_icon(): 
+	var suit_index := card.data.suit # 0..3
+	var region_size := 16.00
+	var spacing := 1
+	
+	# Compute x position
+	var x := suit_index * (region_size + spacing)
+	_suit_atlas.region = Rect2(x, 0, region_size, region_size)
+	suitIcon.texture = _suit_atlas
 
 func _on_card_revealed(card_instance: CardInstance):
 	print("DEBUG: %s of %s revealed" % [
@@ -79,31 +101,31 @@ func _on_card_revealed(card_instance: CardInstance):
 	_update_visual()
 	play_reveal_shake()
 
-func play_idle_animation(_delta: float):
-	_time += _delta * bob_speed
+func play_idle_animation(delta):
+	_time += delta * bob_speed
+	var bob := sin(_time + _phase)
 
-	var y_offset = sin(_time + _phase) * bob_amplitude 
-	var rot = sin(_time * 1 + _phase) * rot_amplitude
-	
-	sides.position = _base_sides_position + Vector2(0, y_offset)
-	sides.rotation_degrees = rot
+	motion_idle.position.y = bob * bob_amplitude
+	motion_idle.rotation_degrees = bob * rot_amplitude
+
+	update_shadow(bob)
 
 
 func play_enter_animation():
 	var offset := enter_amplitude * (-1 if enter_direction == DIRECTIONS.UP else 1)
 
-	root.scale = Vector2(0.6, 0.6)
-	root.position = Vector2(0, offset)
+	motion_base.scale = Vector2(0.6, 0.6)
+	motion_base.position = Vector2(0, offset)
 
 	create_tween() \
 		.set_trans(Tween.TRANS_EXPO) \
 		.set_ease(Tween.EASE_OUT) \
-		.tween_property(root, "scale", Vector2.ONE, 0.2)
+		.tween_property(motion_base, "scale", Vector2.ONE, 0.2)
 
 	create_tween() \
 		.set_trans(Tween.TRANS_EXPO) \
 		.set_ease(Tween.EASE_OUT) \
-		.tween_property(root, "position", Vector2.ZERO, 0.2)
+		.tween_property(motion_base, "position", Vector2.ZERO, 0.2)
 
 func play_reveal_shake():
 	var tween = create_tween()\
@@ -112,32 +134,35 @@ func play_reveal_shake():
 
 	# horizontal shake
 	tween.tween_property(
-		root,
+		motion_base,
 		"position:y",
-		_base_root_position.y + reveal_shake_strength,
+		_base_motion_base_position.y + reveal_shake_strength,
 		reveal_shake_duration * 0.25
 	)
 
 	tween.tween_property(
-		root,
+		motion_base,
 		"position:y",
-		_base_root_position.y - reveal_shake_strength,
+		_base_motion_base_position.y - reveal_shake_strength,
 		reveal_shake_duration * 0.25
 	)
 
 	tween.tween_property(
-		root,
+		motion_base,
 		"position:y",
-		_base_root_position.y,
+		_base_motion_base_position.y,
 		reveal_shake_duration * 0.5
 	)
 
+func update_shadow(bob: float):
+	shadow.rotation_degrees = bob * 4
+
 func animate_layout_delta(delta: Vector2):
 	# Counteract container snap
-	root.position += delta
+	motion_base.position += delta
 	
 	var tween = create_tween()\
 	.set_ease(Tween.EASE_OUT)\
 	.set_trans(Tween.TRANS_EXPO)
 
-	tween.tween_property(root, "position", Vector2.ZERO, 0.2)
+	tween.tween_property(motion_base, "position", Vector2.ZERO, 0.2)
